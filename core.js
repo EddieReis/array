2426adf4f2e6d2b4e4278da1fdc6455cbb746ca8
Ednoelson Reis <46169317+EddieReis@users.noreply.github.com>
2020-10-21 08:18:58 -0300

    Update core.js

--- core.js
+++ core.js
@@ -10,14 +10,13 @@ const vdr = [2, 3, 6, 5];
 
         // document.write ou console.log diz "true" porque está comparando duas variáveis diferentes
         console.log(number, vdr, vd, newArrayNumber);
-        //
-        // document.write ou console.log diz "false" porque está comparando se a variáveis é diferente dela mesma
+
+        // document.write ou console.log diz "false" porque está comparando se duas variáveis diferentes são realmente diferentes (e são).
         console.log(number != number, "Sou um valor falso porque você está me perguntando se eu sou igual a mim mesmo.");
-        // document.write ou console.log diz "verdadeiro" porque você está me comparando a uma variável que entegra valores diferente dela mesma.
+
+        // document.write ou console.log diz "verdadeiro" porque está comparando se a variável "number" é diferente dela mesma (não é).
         console.log(vd != number, "Sou um valor verdadeiro porque desta vez você está me comparando a uma variável que entegra valores diferente de mim mesmo.");
         //end
-        //Run with Node.js
-
-        // By Ednoelson Reis
+        // By Eddie Reis
 
         
