bc2028ef8493a71b7b64ec7a0db05fe44ab750d1
Reis <ednoelsonreis789@hotmail.com>
2020-10-21 07:59:15 -0300

    meeting files

---
+++ core.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const number = [2, 3, 6, 5, 10, 12, 14];
+const vdr = [2, 3, 6, 5];
+        let vd = ["Sou a adição de 1",];
+        let newArrayNumber = ["Sou a multiplicação de 2"];
+
+                for (let i = 0; i < number.length; i++){newArrayNumber.push(number[i] * 2)};
+                for (let i = 0; i < vdr.length; i++){vd.push(number[i] + 1)};
+
+        // document.write ou console.log diz "true" porque está comparando duas variáveis diferentes
+        console.log(number, vdr, vd, newArrayNumber);
+        //
+        // document.write ou console.log diz "false" porque está comparando se a variáveis é diferente dela mesma
+        console.log(number != number, "Sou um valor falso porque você está me perguntando se eu sou igual a mim mesmo.");
+        // document.write ou console.log diz "verdadeiro" porque você está me comparando a uma variável que entegra valores diferente dela mesma.
+        console.log(vd != number, "Sou um valor verdadeiro porque desta vez você está me comparando a uma variável que entegra valores diferente de mim mesmo.");
+        //end
+        //Run with Node.js
+
+        // By Ednoelson Reis
+
+        
